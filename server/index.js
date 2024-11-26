6173de372f13d32122ed37698b4939e3c64a1ff8
Hardik Agarwal <hardikagarwal9308@gmail.com>
2024-11-27 03:56:38 +0530

    Added React for frotend

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ import salesRoutes from "./routes/sales.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(helmet);
+app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
@@ -28,3 +28,19 @@ app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
+
+/* MONGODB CONNECTION */
+const PORT = process.env.PORT || 9000;
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Running On Port: ${PORT}`));
+  })
+  .catch((error) => {
+    console.log(`${error} did not connect`);
+  });
